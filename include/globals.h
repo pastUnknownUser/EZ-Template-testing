b4f521a5e0d38b704802441927db955694db1214
pastUnknownuser <evan6509@gmail.com>
2024-04-01 15:42:45 -0500

    Redo time

--- include/globals.h
+++
@@ -1,9 +0,0 @@
-#include "api.h"
-#include "pros/rotation.hpp"
-
-extern pros::Motor Intake;
-extern pros::ADIDigitalOut intakeRelease;
-extern pros::Motor Cata;
-extern pros::ADIDigitalOut frontWings;
-extern pros::ADIDigitalOut backWings;
-extern pros::ADIDigitalOut Lift;
