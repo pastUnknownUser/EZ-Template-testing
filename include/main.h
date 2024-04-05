3e8b0af89eff024d2f0639ba3973f7cc0a21a60e
pastUnknownUser <evan6509@gmail.com>
2024-04-05 15:40:21 -0500

    Added stuff

--- include/main.h
+++ include/main.h
@@ -41,6 +41,8 @@
  */
 //#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
+#include "autons.hpp"
+#include "globals.h"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
